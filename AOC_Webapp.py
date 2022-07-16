6e85d0fe737a74de134f42d27281288b76f1d81e
Holyfakmyname <64496956+Tanaanan@users.noreply.github.com>
2022-07-16 12:47:59 +0700

    Update AOC_Webapp.py

--- AOC_Webapp.py
+++ AOC_Webapp.py
@@ -2,7 +2,6 @@ import streamlit as st  #Web App
 from PIL import Image, ImageOps #Image Processing
 import time
 from unittest import result
-from pythainlp.util import isthai
 import numpy as np
 from icevision import tfms
 from icevision.models import model_from_checkpoint
